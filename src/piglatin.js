8f63f4604c6a7774762a6270e867c3bf7198fe8b
Desiree <dezlaleman@gmail.com>
2023-08-03 11:33:56 -0400

    polished and reviewed code

--- src/piglatin.js
+++ src/piglatin.js
@@ -1,47 +1,46 @@
-const vowels = ["a", "e", "i", "o", "u"];
-const consonants = [
-  "b",
-  "c",
-  "d",
-  "f",
-  "g",
-  "h",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+// const vowels = ["a", "e", "i", "o", "u"];
 
-const translate = (word) => {
-  word = word.toLowerCase();
-  // if the word's character at index 0 === anything in the vowels array, return wordway!
-  if (vowels.includes(word[0])) {
-    return `${word}way`;
-  } else {
-    let index = 0;
-    // start at index 0, while the current index placement hasn't reached the end- continue, change the index placement to +1;
-    for (let i = 0; i < word.length; i++) {
-      // loops through the word and stores the index of the first vowel it finds!
-      if (vowels.includes(word[i])) {
-        index = i;
-        const firstPart = word.slice(0, index); // start the slice at index 0, end the slice at BUT NOT INCLUDING the index
-        const secondPart = word.slice(index); // start the slice at the index until the end!!
-        return `${secondPart}${firstPart}ay`;
-      }
+const translate = (dialogue) => {
+  dialogue = dialogue.toLowerCase();
+  wordArr = dialogue.split(" "); // this splits the phrase at each space!
+  const vowelsRegex = /[aeiou]/;
+  const pigArray = [];
+  wordArr.forEach((word) => {
+    if (/[0-9@&$#!]/.test(word)) {
+      pigArray.push(word);
+    } else if (vowelsRegex.test(word[0])) {
+      pigArray.push(word + "way");
+    } else {
+      const index = word.search(vowelsRegex);
+      pigArray.push(word.slice(index) + word.slice(0, index) + "ay");
     }
-  }
+  });
+  return pigArray.join(" ");
 };
+
+// const translate = (word) => {
+//   // if the input word has a number or an @ symbol, split back the input unchanged
+//   if (/[0-9@]/.test(word)) {
+//     return word;
+//   }
+//   word = word.toLowerCase();
+//   // if the word's character at index 0 === anything in the vowels array, return wordway!
+//   if (vowels.includes(word[0])) {
+//     return `${word}way`;
+//   } else {
+//     let index = 0;
+//     // start at index 0, while the current index placement hasn't reached the end- continue, change the index placement to +1;
+//     for (let i = 0; i < word.length; i++) {
+//       // loops through the word and stores the index of the first vowel it finds!
+//       if (vowels.includes(word[i])) {
+//         index = i;
+//         const firstPart = word.slice(0, index); // start the slice at index 0, end the slice at BUT NOT INCLUDING the index
+//         const secondPart = word.slice(index); // start the slice at the index until the end!!
+//         return `${secondPart}${firstPart}ay`;
+//       }
+//     }
+//   }
+// };
 
 // const translate = (word) => {
 //  if(vowels.includes(word[0])){
--- tests/piglatin.test.js
+++ tests/piglatin.test.js
@@ -7,9 +7,23 @@ describe("translate function", () => {
     expect(translate("ink")).toBe("inkway");
     expect(translate("ocean")).toBe("oceanway");
     expect(translate("under")).toBe("underway");
+  });
+  test("words that start with Capital letter & consanince", () => {
     expect(translate("Desiree")).toBe("esireeday");
     expect(translate("down")).toBe("ownday");
+  });
+  test("words that start with more than one conanince's", () => {
     expect(translate("space")).toBe("acespay");
     expect(translate("school")).toBe("oolschay");
+  });
+  test("words that have numbers or symbols", () => {
+    expect(translate("189")).toBe("189");
+    expect(translate("hello@grandcircus.co")).toBe("hello@grandcircus.co");
+  });
+  test("A phrase in piglatin!", () => {
+    expect(translate("a quick brown fox")).toBe("away uickqay ownbray oxfay");
+  });
+  test("A phrase in piglatin!", () => {
+    expect(translate("A qUick broWn foX")).toBe("away uickqay ownbray oxfay");
   });
 });
